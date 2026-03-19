d84970eaecb1c0b59bb1fb2ece3f7db2a519a254
Siya Kumari <siyak3858@gmail.com>
2026-03-19 10:53:47 +0530

    Implement usage graph display feature
    Added functionality to display a usage graph based on user inputs.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Load model
 model = pickle.load(open("model.pkl", "rb"))
@@ -13,6 +14,15 @@ avg = st.number_input("Average Usage")
 peak = st.number_input("Peak Usage")
 off_peak = st.number_input("Off Peak Usage")
 variance = st.number_input("Variance")
+
+
+if st.button("Show Usage Graph"):
+    values = [avg, peak, off_peak, variance]
+    labels = ["Avg", "Peak", "Off-Peak", "Variance"]
+
+    plt.figure()
+    plt.bar(labels, values)
+    st.pyplot(plt)
 
 if st.button("Predict"):
     features = np.array([[avg, peak, off_peak, variance]])
